c534e699552c483f5facb8e05534d421a66f4f7d
Vaibhav0745 <vaibhavmendake25@gmail.com>
2025-03-12 19:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -513,3 +513,15 @@ videoCards.forEach(card => {
     card.classList.remove('hover');
   });
 });
+
+// Disable right-click context menu
+        document.addEventListener('contextmenu', function(event) {
+            event.preventDefault();
+        });
+
+        // Disable Ctrl key combinations
+        document.addEventListener('keydown', function(event) {
+            if (event.ctrlKey) {
+                event.preventDefault();
+            }
+        });
